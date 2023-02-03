8547e9bfa7aeff09fc2621c0d641cdb5061010a1
Caique Sales de Miranda Gomes <80981281+kiqsmg@users.noreply.github.com>
2023-02-03 15:13:38 -0300

    Create contagem_notas.java
    Devido a dificuldade de adicionar o código de contagem individual das notas ao main, estou criando um file separado para testar essa função a parte.

---
+++ contagem_notas.java
@@ -0,0 +1,4 @@
+//Teste para erar código que faça um tracking das notas de 10, 20, 50 e 100 reais
+
+private static int[] notas = {100, 100, 100, 100, 50, 50, 20, 20, 10, 10};
+private static int[] fluxoNotas= new int[notas.length];
